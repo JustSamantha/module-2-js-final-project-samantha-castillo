5d126a337883816946d36612823ee3fb94fe426b
Samantha Castillo <samanthacas@pm.me>
2025-09-20 14:37:58 -0700

    - Adds a gitignore - Adds dialog to edit/add bookmarks - Adds more styles - Adds logic to open dialogs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,32 @@
-const h1 = document.querySelector('h1');
+const bkmrkDialog = document.querySelector('dialog');
+const bkmrkDialogTitle = document.querySelector('dialog h2');
+const bkmrkFormURL = document.querySelector('#bookmarkURL');
+const bkmrkFormName = document.querySelector('#bookmarkName');
+const bkmrkBtn = document.querySelector('#addBookmarkBtn');
+const closeBkmrkDialogBtn = document.querySelector('#closeBookmarkDialogBtn');
+const submitBkmrkDialogBtn = document.querySelector('#submitBookmarkDialogBtn');
+const editBookmarkBtns = document.querySelectorAll('.editBookmarkBtn');
 
-console.log('Bookmarks Manager extension loaded');
-console.log('h1: ', h1);
+bkmrkBtn.addEventListener('click', (e) => {
+  bkmrkDialogTitle.textContent = 'Add Bookmark';
+  submitBkmrkDialogBtn.textContent = 'Add';
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    bkmrkFormURL.value = tabs[0].url;
+    bkmrkFormName.value = tabs[0].title;
+    bkmrkDialog.showModal();
+  });
+});
 
-const addBookmarkDialog = document.querySelector('dialog');
-const addBookmarkBtn = document.querySelector('#addBookmarkBtn');
+closeBkmrkDialogBtn.addEventListener('click', (e) => {
+  bkmrkDialog.close();
+});
 
-addBookmarkBtn.addEventListener('click', (e) => {
-  addBookmarkDialog.showModal();
+function editBookmarkAction(e) {
+  bkmrkDialogTitle.textContent = 'Edit Bookmark';
+  submitBkmrkDialogBtn.textContent = 'Save';
+  bkmrkDialog.showModal();
+}
+
+editBookmarkBtns.forEach((editBookmarkBtn) => {
+  editBookmarkBtn.addEventListener('click', editBookmarkAction);
 });
